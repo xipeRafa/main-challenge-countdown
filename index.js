bd8104d24a18eefe79b5ce5766c9354b5e525fc0
rafa <rafaelsistem87@gmail.com>
2021-04-05 15:24:35 -0700

    clean2

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-(function seconds() {
-
+(function () {  //immediately invoked function expression (IIFE) 
       let count  = 59
       let second = document.getElementById('second')
       
@@ -7,21 +6,27 @@
         let seconds;
         
         if (count > 0) {
-          count -= 1
-          seconds = count < 10 ? `0${count}` : `${count}`;
-          second.querySelector('.card-face-back').textContent = seconds;
+          count -= 1  //rest 1 in every loop
+          
+          seconds = count 
+          second.querySelector('.card-face-back').textContent = seconds
+
+          /* add second in  card-face-front(div) */
           second.dataset.baseAfter = seconds;
-          second.querySelector('.card').classList.add('flip');
+
+          second.querySelector('.card').classList.add('flip')
+
           second.querySelector('.card').addEventListener('animationend', event => {
-            if(event.animationName === 'flip') {
-              second.querySelector('.card-face-front').textContent = seconds;
+            if(event.animationName === 'flip') { 
+              // function start when animation start
+              second.querySelector('.card-face-front').textContent = seconds
+
+              /* add second in  card-face-Before(div) */
               second.dataset.baseBefore = seconds;
-              second.querySelector('.card').classList.remove('flip');
+              second.querySelector('.card').classList.remove('flip')
             }
           })
-        } else {
-          count = 59;
-        }
+        } else { count = 59 }
       }
     
        setInterval(() => {
@@ -31,34 +36,32 @@
    
     
 
-  (function minutes() {
+  (function () {
 
       let count = 59
       let minute = document.getElementById('minute')
 
-      function countdown() {
+      function countdown2() {
      
      let minutes;
  
     if (count > 0) {
       count -= 1
-      minutes = count < 10 ? `0${count}` : `${count}`;
-      minute.querySelector('.card-face-back').textContent = minutes;
-      minute.dataset.baseAfter = minutes;
-      minute.querySelector('.card').classList.add('flip');
+      minutes = count
+      minute.querySelector('.card-face-back').textContent = minutes
+      minute.dataset.baseAfter = minutes
+      minute.querySelector('.card').classList.add('flip')
       minute.querySelector('.card').addEventListener('animationend', event => {
         if(event.animationName === 'flip') {
-          minute.querySelector('.card-face-front').textContent = minutes;
-          minute.dataset.baseBefore = minutes;
-          minute.querySelector('.card').classList.remove('flip');
+          minute.querySelector('.card-face-front').textContent = minutes
+          minute.dataset.baseBefore = minutes
+          minute.querySelector('.card').classList.remove('flip')
         }
       })
-    } else {
-      count = 59;
-    }
+    } else { count = 59 }
   }
 
    setInterval(() => {
-         countdown();
+         countdown2();
        }, 60000);
 })();
